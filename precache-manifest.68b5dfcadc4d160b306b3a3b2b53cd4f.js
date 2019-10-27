bdb9d368e8ed0bde226d3165859c0ade6d3cfa4a
Kathleen Leung <kathleenleungky@gmail.com>
2019-10-26 17:21:57 -0700

    Updates

---
+++ precache-manifest.68b5dfcadc4d160b306b3a3b2b53cd4f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e19b235518811b5f1e95870bc57a1bad",
+    "url": "/stclicky-game/index.html"
+  },
+  {
+    "revision": "5bc70401d955ca57622b",
+    "url": "/stclicky-game/static/css/main.273e45f4.chunk.css"
+  },
+  {
+    "revision": "9702356ce22916f0ecf8",
+    "url": "/stclicky-game/static/js/2.14f97f3e.chunk.js"
+  },
+  {
+    "revision": "5bc70401d955ca57622b",
+    "url": "/stclicky-game/static/js/main.5ab513fc.chunk.js"
+  },
+  {
+    "revision": "fec2e68718cabba0b218",
+    "url": "/stclicky-game/static/js/runtime-main.b5479acd.js"
+  }
+]);
